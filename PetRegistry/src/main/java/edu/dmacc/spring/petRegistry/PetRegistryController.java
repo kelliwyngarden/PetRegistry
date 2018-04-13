03663df6f4a3cb6ae0f58b534326ccbb25eec9ff
sntaylor94 <taylor.shannon.n@gmail.com>
2018-04-13 11:18:10 -0500

    Pet Registry Edit/Delete start

--- PetRegistry/src/main/java/edu/dmacc/spring/petRegistry/PetRegistryController.java
+++ PetRegistry/src/main/java/edu/dmacc/spring/petRegistry/PetRegistryController.java
@@ -1,6 +1,9 @@
 package edu.dmacc.spring.petRegistry;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,8 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PetRegistryController {
 	
-	@Autowired PetDao petDao();
-	@Autowired OwnerDao ownerDao();
+	@Autowired PetDao petDao;
+	@Autowired OwnerDao ownerDao;
 	
 	@RequestMapping(value = "/homepage")
 	public ModelAndView owner() {
@@ -32,6 +35,45 @@ public class PetRegistryController {
 		petDao.insertPet(pet);
 		modelAndView.setViewName("petResult");
 		modelAndView.addObject("p", pet);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/viewAllPets")
+	public ModelAndView viewAllPets() {
+		ModelAndView modelAndView = new ModelAndView();
+		List<Pet> allPets = petDao.getAllPets();
+		modelAndView.setViewName("viewAllPets");
+		modelAndView.addObject("allPets", allPets);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/editPet")
+	public ModelAndView editPet(int petId) {
+		ModelAndView modelAndView = new ModelAndView();
+		Pet pet = petDao.getPet(petId);
+		modelAndView.setViewName("editPetForm");
+		modelAndView.addObject("p", pet);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/editPetResult")
+	public ModelAndView editPetResult(Pet pet) {
+		ModelAndView modelAndView = new ModelAndView();
+		petDao.updatePet(pet);
+		List<Owner> allOwners = ownerDao.getAllOwners();
+		modelAndView.setViewName("editPetResult");
+		modelAndView.addObject("p", pet);
+		modelAndView.addObject("allOwners", allOwners);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/deletePet")
+	public ModelAndView deletePet(int petId) {
+		ModelAndView modelAndView = new ModelAndView();
+		petDao.removePet(petId);
+		List<Pet> allPets = petDao.getAllPets();
+		modelAndView.setViewName("viewAllPets");
+		modelAndView.addObject("allPets", allPets);
 		return modelAndView;
 	}
 	
---
+++ PetRegistry/src/main/webapp/WEB-INF/pages/viewAllPets.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="mvc"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>View All Pets</title>
+</head>
+<body>
+<mvc:form modelAttribute = "post" action = "deletePet.mvc">
+<mvc:form modelAttribute = "post" action = "editPet.mvc">
+	<table>
+		<tr>
+		<th>Pet Id</th>
+			<th>Pet Name</th>
+			<th>Pet Species</th>
+			<th>Pet Age</th>
+			<th>Pet Coloring</th>
+			<th>Owner Name</th>
+		</tr>
+		<c:forEach items = "${allPets}" var = "pet">
+			<tr>
+				<td><input type = "radio" name = "petId" value = "${pet.petId}">${pet.petId}</td>
+				<td>${pet.name}</td>
+				<td>${pet.species}</td>
+				<td>${pet.age}</td>
+				<td>${pet.coloring}</td>
+				<td>${pet.owner.firstName} ${pet.owner.lastName}</td>
+			</tr>
+		</c:forEach>
+	</table>
+	<input type = "submit" value = "Edit Pet" >
+	</mvc:form>
+	<input type = "submit" value = "Remove Pet">
+</mvc:form>
+</body>
+</html>
